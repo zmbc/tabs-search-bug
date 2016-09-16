4cac38cb040645c908eee3a3dff79edaae9a1b85
Zeb Burke-Conte <zeb@schedulista.com>
2016-09-16 14:17:34 -0700

    initial commit

---
+++ app/pages/tabs/tabs.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { HomePage } from '../home/home';
+
+@Component({
+  templateUrl: 'build/pages/tabs/tabs.html'
+})
+export class TabsPage {
+
+  public tab1Root: any;
+  public tab2Root: any;
+  public tab3Root: any;
+
+  constructor() {
+    // this tells the tabs component which Pages
+    // should be each tab's root Page
+    this.tab1Root = HomePage;
+  }
+}
